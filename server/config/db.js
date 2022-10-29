7e97a54054e1c06005054f491f4793d75299a90e
bhoang <baobinhbuffon@gmail.com>
2022-10-28 21:04:57 -0400

    midterm

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+   "Secret": 'SomeSecret'
+};
